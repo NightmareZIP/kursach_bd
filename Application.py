5d7c2ced56e7d4348c25b57e930f257aca467d84
Victor <31132241+NightmareZIP@users.noreply.github.com>
2021-12-16 18:38:29 +0300

    Рабочая версия

--- Application.py
+++ Application.py
@@ -1,18 +1,94 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from functools import partial
+import sql_test
 
 
 class Text_window(ttk.Frame):
-    def __init__(self, master, *args, is_input=False, func=None, **kwargs):
+    def __init__(self, master, DB, *args, left_menu, options, **kwargs):
         super().__init__(master, *args, **kwargs)
-        if True or is_input:
-            message = StringVar()
-            inp = Entry(self, textvariable=message)
-            inp.pack()
-        l_res = Text(self)
-        l_res.insert(END, 'AAAAAAAAAAA')
-        l_res.pack()
+        self.DB = DB
+        self.options = options
+        self.master = master
+        self.command = options['command']
+        self.text_w = False
+        if options['is_input']:
+
+            self.create_input_area()
+        else:
+            self.execute(self.options['command'])
+
+    def execute(self, formated_command):
+        # print(formated_command)
+        if (self.text_w):
+            self.l_res.pack_forget()
+            self.l_res.destroy()
+
+        self.text_w = True
+        columns, res = self.DB.execute(formated_command)
+
+        self.l_res = ttk.Treeview(self, show="headings")
+        self.l_res['columns'] = columns
+
+        for i in columns:
+            self.l_res.column(i, anchor=CENTER)
+            self.l_res.heading(i, text=i)
+
+        ysb = Scrollbar(self, orient=VERTICAL, command=self.l_res.yview)
+        self.l_res.configure(yscroll=ysb.set)
+        for i in res:
+            self.l_res.insert("", END, values=i)
+
+        self.l_res.pack(fill=BOTH, expand=1)
+
+    def create_input_area(self):
+        f = Frame(self)
+        if (self.options['is_input']['select']):
+            q_d = self.options['is_input']['select']
+            ent = q_d['entity']
+            fields = q_d['fields']
+            self.checkboxes = q_d['checkboxes']
+            lab = Label(f, text=q_d['label'])
+            lab.pack(side=TOP)
+            if self.checkboxes:
+                c_f = Frame(f)
+                self.v = StringVar(c_f, "1")
+                for (text, val) in self.checkboxes:
+                    Radiobutton(c_f, text=text, variable=self.v,
+                                value=val).pack(side=LEFT)
+                c_f.pack(side=TOP)
+            querry_f = ', '.join(fields)
+            querry = "SELECT {} FROM {}".format(querry_f, ent)
+            h, res = self.DB.execute(querry)
+
+            for elem in res:
+                elem = list(map(str, elem))
+                data = [elem[0]]
+                action_with_arg = partial(self.run_formated_command,
+                                          self.command, data)
+
+                r = Button(f,
+                           text=' '.join(list(elem)),
+                           command=action_with_arg)
+                r.pack(side=TOP, fill=BOTH, expand=1)
+
+        # self.inp = Entry(f)
+        # self.inputs = [self.inp]
+        # b = Button(f,
+        #            text='Ввод',
+        #            command=lambda: self.run_formated_command(
+        #                self.command, self.inputs))
+        # self.inp.pack(side=LEFT)
+        # b.pack(side=RIGHT)
+        f.pack(side=LEFT)
+
+    def run_formated_command(self, command, inputs):
+        self.inputs = inputs[:]
+        if self.checkboxes: self.inputs.append(self.v.get())
+        f_command = command.format(*self.inputs)
+
+        self.execute(f_command)
 
     # def run_mod(self):
     #     self.mod.pack_forget()
@@ -53,8 +129,11 @@ class Text_window(ttk.Frame):
 
 
 class Types(ttk.Frame):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, *args, DB, **kwargs):
+
         super().__init__(master, *args, **kwargs)
+        self.DB = DB
+        self.is_w = False
         self.master = master
         self.mod_f = Frame(master)
 
@@ -62,14 +141,111 @@ class Types(ttk.Frame):
         #style.configure('lefttab.TNotebook', tabposition='wn')
         #notebook = ttk.Notebook(master, style='lefttab.TNotebook')
         #maximum = max([len(i) for i in modules.keys()])
-        l_f = Frame(self)
+        l_f = Frame(self, background='red')
+        # scrollbar = Scrollbar(self)
+        queries = {
+            "Список услуг": {
+                'command':
+                "SELECT name, price FROM services GROUP BY name, price;",
+                'is_input': False
+            },
+            "Список машин": {
+                'command': "SELECT * FROM cars;",
+                'is_input': False
+            },
+            "Информация о машине": {
+                'command': """SELECT * FROM services
+                                WHERE id IN	(SELECT service_id FROM order_services
+                                    WHERE order_id IN (SELECT id FROM orders
+                                        WHERE  car_id = (SELECT id FROM cars
+                                                                WHERE id = '{}')));""",
+                'is_input': {
+                    'select': {
+                        'label': 'Выберите машину',
+                        'entity': 'cars',
+                        'fields': ['id', 'name', 'car_number'],
+                        'checkboxes': (())
+                    }
+                }
+            },
+            "Информация о работе мастера за период": {
+                'command':
+                """ SELECT orders.id, services.name, services.price, orders.creation_date, orders.final_date
+                    FROM services, orders, (SELECT service_id, order_id 
+						 FROM order_services  
+						WHERE master_id = (SELECT id 
+										   FROM masters
+										    WHERE masters.id = {})
+						) AS cur_s_o
+                    WHERE services.id = cur_s_o.service_id
+                    AND
+                    orders.id = cur_s_o.order_id
+                    AND 
+                    orders.final_date IS NOT NULL
+                    AND 
+                    orders.final_date BETWEEN DATE_SUB(NOW(), INTERVAL {} DAY) AND NOW() ;""",
+                'is_input': {
+                    'select': {
+                        'label':
+                        'Выберите период и мастера',
+                        'entity':
+                        'masters',
+                        'fields': ['id', 'name', 'second_name', 'last_name'],
+                        'checkboxes': (('день', 1), ('месяц', 30),
+                                       ('квартал', 91), ('год', 365))
+                    }
+                }
+            },
+            "Рассчет стоимости услуг": {
+                'command':
+                """SELECT  o_c_s.order_id, o_c_s.car_number, services.name, SUM(services.price) as sum  
+                                            FROM services, (SELECT order_services.service_id, order_car.order_id, order_car.name, order_car.car_number 
+                                                            FROM order_services, (SELECT orders.id as order_id , selected_cars.name, selected_cars.car_number 
+                                                                                FROM orders, (SELECT  cars.id, cars.name, cars.car_number  
+                                                                                                FROM cars
+                                                                                                WHERE cars.id IN (SELECT id 
+                                                                                                                FROM cars 
+                                                                                                                WHERE owner_id IN (SELECT id 
+                                                                                                                                    FROM clients
+                                                                                                                                    WHERE clients.id = {}))
+                                                                                            ) AS selected_cars
+                                                                                WHERE orders.car_id = selected_cars.id
+                                                                                ) AS order_car
+                                                            WHERE order_car.order_id = order_services.order_id) AS o_c_s
+                                                WHERE services.id = o_c_s.service_id
+                                                GROUP BY o_c_s.order_id;""",
+                'is_input': {
+                    'select': {
+                        'label': 'Выберите клиента',
+                        'entity': 'clients',
+                        'fields': ['id', 'name', 'second_name', 'last_name'],
+                        'checkboxes': (())
+                    }
+                }
+            },
+        }
+        buttons = []
+        for button in queries.keys():
+            # print(queries[button])
+            opt = queries[button]
+            action_with_arg = partial(self.create_request_section, opt)
+            r = Button(l_f, text=button, command=action_with_arg)
+            # r = Radiobutton(l_f, text = 'AAA', variable = t, value = i, indicatoron=0, command = self.new_list )
+            r.pack(side=TOP, fill=BOTH, expand=1)
 
-        r = Button(l_f, text="Машинки", command=lambda: messagebox.showinfo(
-            "Hello Python", "НАЖАЛЪ"))
-        # r = Radiobutton(l_f, text = 'AAA', variable = t, value = i, indicatoron=0, command = self.new_list )
-        r.pack(fill=BOTH, side=TOP, expand=1, anchor=W)
+        l_f.pack(side=LEFT, fill=BOTH)
 
-        l_f.pack(fill=BOTH, side=LEFT, expand=1, )
+    def create_request_section(self, options):
+        # print(options)
+        if self.is_w:
+            self.w.pack_forget()
+            self.w.destroy()
+        self.w = Text_window(self.master,
+                             DB=self.DB,
+                             left_menu=self,
+                             options=options)
+        self.w.pack(fill=BOTH, expand=1, side=RIGHT)
+        self.is_w = True
 
     # def new_list(self):
     #     print("called")
@@ -80,19 +256,23 @@ class Types(ttk.Frame):
 
 
 class Main_application(ttk.Frame):
-
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, DB, *args, **kwargs):
 
         super().__init__(master, *args, **kwargs)
 
         self.master = master
 
-        f = Types(self)
-        f.pack(fill=BOTH, side=LEFT, anchor=W)
-        w = Text_window(self)
-        w.pack(fill=BOTH, side=RIGHT, anchor=W)
+        f = Types(self, DB=db)
+        f.pack(fill=BOTH, side=LEFT)
 
 
+data = {
+    'host': "127.0.0.1",
+    'user': "root",
+    'password': "qwerty123",
+    'database': "autoservice"
+}
+db = sql_test.DB(data)
 root = Tk()
 root.title("Автосервис")
 #root.attributes('-fullscreen', True)
@@ -102,6 +282,6 @@ m = StringVar()
 # root.geometry('400x400')
 root.minsize(800, 400)
 
-app = Main_application(root)
+app = Main_application(root, DB=db)
 app.pack(fill=BOTH, expand=1)
 root.mainloop()
--- sql_test.py
+++ sql_test.py
@@ -1,21 +1,49 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="Batman74",
-  database="autoservice"
-)
-mycursor = mydb.cursor()
+
+class DB:
+    def __init__(self, data):
+        self.is_error = False
+        try:
+            self.mydb = mysql.connector.connect(
+                **data
+                # host="127.0.0.1",
+                # user="root",
+                # password="Batman74",
+                # database="autoservice"
+            )
+            self.mycursor = self.mydb.cursor()
+        except Error as e:
+            self.is_error = True
+            return send_error(error)
+
+    def send_error(self, error):
+        return error
+
+    def execute(self, command):
+        self.mycursor.execute(command)
+        myresult = self.mycursor.fetchall()
+        res = ''
+        headers = self.mycursor.column_names
+        # headers = "{:<25}"*len(self.mycursor.column_names)
+        # headers = headers.format(*list(self.mycursor.column_names))+'\n'
+        # for i in myresult:
+        #     row = "{:<25}"*len(i)
+        #     res += row.format(*list(i))+'\n'
+        return headers, myresult
+
+#   print(x)
+
 # mycursor.execute("SHOW DATABASES")
 
+
 # for x in mycursor:
-#   print(x) 
+#   print(x)
 # mycursor.execute("CREATE DATABASE autoservice")
 #mycursor.execute("CREATE TABLE clients (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), second_name VARCHAR(255), last_name VARCHAR(255), phone VARCHAR(255))")
 #mycursor.execute("SELECT * FROM clients")
-mycursor.execute("SHOW TABLES")
-#myresult = mycursor.fetchall()
+# mycursor.execute("SHOW TABLES")
+# #myresult = mycursor.fetchall()
 
-for x in mycursor:
-  print(x)
+# for x in mycursor:
+#     print(x)
