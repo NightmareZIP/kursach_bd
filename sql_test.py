f3a708334f6ff3e2fe21dcd4284d88d8e51a0c97
Victor <31132241+NightmareZIP@users.noreply.github.com>
2021-12-16 19:32:27 +0300

    убрал лишнее

--- sql_test.py
+++ sql_test.py
@@ -25,25 +25,5 @@ class DB:
         myresult = self.mycursor.fetchall()
         res = ''
         headers = self.mycursor.column_names
-        # headers = "{:<25}"*len(self.mycursor.column_names)
-        # headers = headers.format(*list(self.mycursor.column_names))+'\n'
-        # for i in myresult:
-        #     row = "{:<25}"*len(i)
-        #     res += row.format(*list(i))+'\n'
+
         return headers, myresult
-
-#   print(x)
-
-# mycursor.execute("SHOW DATABASES")
-
-
-# for x in mycursor:
-#   print(x)
-# mycursor.execute("CREATE DATABASE autoservice")
-#mycursor.execute("CREATE TABLE clients (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), second_name VARCHAR(255), last_name VARCHAR(255), phone VARCHAR(255))")
-#mycursor.execute("SELECT * FROM clients")
-# mycursor.execute("SHOW TABLES")
-# #myresult = mycursor.fetchall()
-
-# for x in mycursor:
-#     print(x)
